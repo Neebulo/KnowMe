1c609e5c3f065a7ff391e3f0dee39d9510ccf8cb
Cris <rhapsodyshadow@yahoo.com>
2015-07-27 02:14:04 -0700

    added create method and private for comments controller, form code added to views/post/show.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to post_path(@comment.post)
+  end
+
+private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
 end
