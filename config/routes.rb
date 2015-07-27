2e2098481dbe3ca944b367540a3f9d0a7d1e8085
Cris <rhapsodyshadow@yahoo.com>
2015-07-26 22:03:29 -0700

    generated models POST and COMMENT, with POSTS controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/show'
+
+  get 'posts/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
