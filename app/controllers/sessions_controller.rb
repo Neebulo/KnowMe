46bc67bb82f84928420aa9e6dffcb183d99464eb
Cris <rhapsodyshadow@yahoo.com>
2015-07-27 20:10:32 -0700

    added create destroy to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.where(email: user_params[:email]).first
+
+    if @user && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to new_session_path  #render 'new' <<try this out
+    end
+
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
