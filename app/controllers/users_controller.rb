b67540832609b35a4dfc0272403ecd9164ba8233
Cris <rhapsodyshadow@yahoo.com>
2015-07-27 19:47:49 -0700

    added new and create code in users controller plus private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new
-  end
+      @user = User.new
+    end
 
-  def create
+    def create
+      @user = User.new user_params
+      if @user.save
+        redirect_to "/"
+      else
+        render "new"
+      end
+    end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
   end
-end
