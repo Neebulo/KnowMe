7ca86fa364a3481d4c233d862f59be8650bdb59a
Cris <rhapsodyshadow@yahoo.com>
2015-07-27 18:53:09 -0700

    added has_secure_password to user model, did rake:db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727044321) do
+ActiveRecord::Schema.define(version: 20150727091631) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20150727044321) do
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
